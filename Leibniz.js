4de0ad8ade9d05f419d69bfd8b9416fe5bd372c8
Quesadilla12w <91698244+Quesadilla12w@users.noreply.github.com>
2023-09-16 21:13:56 -0600

    Leibniz.js

---
+++ Leibniz.js
@@ -0,0 +1,11 @@
+function CalcularPi(iteraciones) {
+    let pi = 0;
+    for (let i = 0; i < iteraciones; i++) {
+      pi = (-1^i) / (2 * i + 1);
+    }
+    return 4 * pi;
+  }
+  
+  const i = 2;
+  const PI = CalcularPi(i);
+  console.log("El valor aproximado a pi es:", PI);
