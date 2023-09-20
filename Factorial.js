0f229e2125c5be754f1b8cdba4fe784a4a1b302a
Quesadilla12w <91698244+Quesadilla12w@users.noreply.github.com>
2023-09-19 19:47:19 -0600

    Update Factorial.js

--- Factorial.js
+++ Factorial.js
@@ -1,14 +1,14 @@
 function Factorial(numero) {  
   if (numero == 1 || numero == 0){
-    return 1
-  } 
+    return  1
+  } else {
   let resultado = 1; 
   for (let i = 1; i <= numero; i++) {
     resultado = resultado*i; 
   }
   return resultado;
-}
+}}
 
-const num = 6;
+const num = 0;
 const fac = Factorial(num);
 console.log("El factorial es:", fac);
