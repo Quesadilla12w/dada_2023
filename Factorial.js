f352f9f2b9370ad8a89f69167932658ebe90c501
Quesadilla12w <91698244+Quesadilla12w@users.noreply.github.com>
2023-09-24 19:29:59 -0600

    Con recursividad Factorial.js

--- Factorial.js
+++ Factorial.js
@@ -2,13 +2,11 @@ function Factorial(numero) {
   if (numero == 1 || numero == 0){
     return  1
   } else {
-  let resultado = 1; 
-  for (let i = 1; i <= numero; i++) {
-    resultado = resultado*i; 
+    resultado = numero * Factorial(numero-1)
   }
   return resultado;
-}}
+}
 
-const num = 0;
+const num = 5;
 const fac = Factorial(num);
 console.log("El factorial es:", fac);
